2a5f9871491168d6dabb5ce1052cf6e91d1ff553
Juan Luis Romero <juanlu_21@hotmail.com>
2021-10-21 01:21:35 +0200

    First stable commit of first view

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,26 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { Container } from '@mui/material';
 import Characters from './containers/Characters';
-
+import Header from './containers/Header';
+/*
+TODO list
+  Add an error boundary
+  Add a theme
+  Do the request
+  Create interfaces
+  Create routes
+  Study if developer options works
+  Study deploy
+  Create or modify README.md
+*/
 function App() {
   return (
-    <Container maxWidth={'xl'}>
-      <Characters />
-    </Container>
+    <>
+      <Header />
+      <Container maxWidth={'xl'}>
+        <Characters />
+      </Container>
+    </>
   );
 }
 
