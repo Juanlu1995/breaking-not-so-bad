d507c0f6fd2e4e22a5c9ff65efb4e17f614acc65
Juan Luis Romero <juanlu_21@hotmail.com>
2021-10-23 00:01:43 +0200

    Load details

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Container } from '@mui/material';
+import { Container, createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material';
 import Header from './containers/Header';
 import { BrowserRouter, Route, RouteProps } from 'react-router-dom';
 import { routes } from './routes';
+import { ThemeProvider } from 'styled-components';
 
 /*
 TODO list
@@ -18,22 +19,27 @@ TODO list
   Create or modify README.md
 */
 function App() {
+  const theme = createTheme();
   return (
     <>
-      <Header />
-      <Container maxWidth={'xl'}>
-        <BrowserRouter>
-          {routes.map((r: RouteProps, index: number) => (
-            <Route
-              exact={r?.exact}
-              component={r?.component}
-              strict={r?.strict}
-              path={r?.path}
-              key={`route_${index}`}
-            />
-          ))}
-        </BrowserRouter>
-      </Container>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Header />
+          <Container maxWidth={'xl'}>
+            <BrowserRouter>
+              {routes.map((r: RouteProps, index: number) => (
+                <Route
+                  exact={r?.exact}
+                  component={r?.component}
+                  strict={r?.strict}
+                  path={r?.path}
+                  key={`route_${index}`}
+                />
+              ))}
+            </BrowserRouter>
+          </Container>
+        </ThemeProvider>
+      </MuiThemeProvider>
     </>
   );
 }
